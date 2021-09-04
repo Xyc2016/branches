8771da4e8ca5be2f27e1da978688bbc7969d3d6a
xuyucai <xuyucai@meideng.net>
2021-09-04 20:44:48 +0800

    MOD: Started

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,23 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponentComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponentComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponentComponent },
+      { path: 'cart', component: CartComponent},
+      { path: 'shipping', component: ShippingComponentComponent}
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponentComponent,
+    CartComponent,
+    ShippingComponentComponent
   ],
   bootstrap: [
     AppComponent
---
+++ my-app/src/app/cart/cart.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss']
+})
+export class CartComponent implements OnInit {
+  items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  })
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('You order has been subbmitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+
+}
